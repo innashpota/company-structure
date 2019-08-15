40d39d2a8a9afbcbdaddcbe2bdd202050d8c02ff
Inna <shpota_inna@ukr.net>
2019-08-15 22:54:59 +0300

    Init Spring Boot project

---
+++ src/main/java/com/inna/shpota/companystructure/CompanyStructureApplication.java
@@ -0,0 +1,13 @@
+package com.inna.shpota.companystructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyStructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inna/shpota/companystructure/CompanyStructureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.inna.shpota.companystructure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyStructureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

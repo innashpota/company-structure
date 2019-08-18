4119018fe0f946355ee9f9dde19c0753fc4ada15
Inna <shpota_inna@ukr.net>
2019-08-18 17:17:30 +0300

    Added Employee service

---
+++ src/main/java/com/inna/shpota/company/service/EmployeeService.java
@@ -0,0 +1,32 @@
+package com.inna.shpota.company.service;
+
+import com.inna.shpota.company.entity.Employee;
+import com.inna.shpota.company.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository repository;
+
+    public EmployeeService(EmployeeRepository repository) {
+        Assert.notNull(repository, "Repository must not be null");
+        this.repository = repository;
+    }
+
+    public void add(Employee employee) {
+        repository.save(employee);
+    }
+
+    public void edit(Employee updated) {
+        repository.save(updated);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Iterable<Employee> showAll() {
+        return repository.findAll();
+    }
+}

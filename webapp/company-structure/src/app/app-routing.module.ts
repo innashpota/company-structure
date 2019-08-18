dc1ec8fb63ba1f6209485722d8151935d1695abf
Inna <shpota_inna@ukr.net>
2019-08-18 20:33:57 +0300

    Added Employee and PageNotFound components

--- webapp/company-structure/src/app/app-routing.module.ts
+++ webapp/company-structure/src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {EmployeesComponent} from './employees/employees.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employees',
+    component: EmployeesComponent
+  },
+  {
+    path: '',
+    redirectTo: '/employees',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- webapp/company-structure/src/app/app.module.ts
+++ webapp/company-structure/src/app/app.module.ts
@@ -5,10 +5,14 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MaterialModule} from './material/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {EmployeesComponent} from './employees/employees.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,

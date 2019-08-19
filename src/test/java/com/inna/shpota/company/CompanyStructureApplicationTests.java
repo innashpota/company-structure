c2c768bb7c1f67a2cd75e96c14f63ed0d11d9a12
Inna <shpota_inna@ukr.net>
2019-08-19 22:24:26 +0300

    Fixed problem with H2 for tests

--- src/test/java/com/inna/shpota/company/CompanyStructureApplicationTests.java
+++ src/test/java/com/inna/shpota/company/CompanyStructureApplicationTests.java
@@ -1,20 +1,17 @@
 package com.inna.shpota.company;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Profile("test")
-@Ignore
+@ActiveProfiles("test")
 public class CompanyStructureApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }

e12d434288c262e7bac377d59d2fe0b7cfa7e927
Inna <shpota_inna@ukr.net>
2019-09-01 21:32:22 +0300

    Added navigation components

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'company-structure';
+export class AppComponent implements OnDestroy {
+  title = 'Company structure';
+  mobileQuery: MediaQueryList;
+  readonly mobileQueryListener: () => void;
+
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this.mobileQueryListener);
+  }
 }

1f83bb0f4e264e9135bb4fab2ef8d090a8da387a
Inna <shpota_inna@ukr.net>
2019-09-13 21:00:16 +0300

    Added should create to AddEmployeeToProjectComponent

--- webapp/src/app/projects/dialogs/add-employee-to-project/add-employee-to-project.component.spec.ts
+++ webapp/src/app/projects/dialogs/add-employee-to-project/add-employee-to-project.component.spec.ts
@@ -1,16 +1,63 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Router} from '@angular/router';
 
 import {AddEmployeeToProjectComponent} from './add-employee-to-project.component';
+import {MaterialModule} from '../../../material/material.module';
+import {AddProjectComponent} from '../add-project/add-project.component';
+import {EditProjectTitleComponent} from '../edit-project-title/edit-project-title.component';
+import {EditEmployeeInProjectComponent} from '../edit-employee-in-project/edit-employee-in-project.component';
+import {DeleteProjectComponent} from '../delete-project/delete-project.component';
+import {EmployeesComponent} from '../../../employees/employees.component';
+import {ProjectComponent} from '../../project.component';
+import {PageNotFoundComponent} from '../../../page-not-found/page-not-found.component';
+import {ProjectService} from '../../project.service';
 
 describe('AddEmployeeToProjectComponent', () => {
   let component: AddEmployeeToProjectComponent;
   let fixture: ComponentFixture<AddEmployeeToProjectComponent>;
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AddEmployeeToProjectComponent]
-    })
-      .compileComponents();
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MaterialModule
+      ],
+      declarations: [
+        AddEmployeeToProjectComponent,
+        AddProjectComponent,
+        DeleteProjectComponent,
+        EditEmployeeInProjectComponent,
+        EditProjectTitleComponent,
+        EmployeesComponent,
+        ProjectComponent,
+        PageNotFoundComponent
+      ],
+      providers: [
+        ProjectService,
+        {
+          provide: Router,
+          useValue: mockRouter
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {data: 'Project name'}
+        }
+      ]
+    });
+    component = TestBed.get(MatDialog);
   }));
 
   beforeEach(() => {
@@ -21,5 +68,9 @@ describe('AddEmployeeToProjectComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });

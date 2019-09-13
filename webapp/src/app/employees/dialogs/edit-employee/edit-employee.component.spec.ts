9df0979ac6605b749629800ae929e243674ad21e
Inna <shpota_inna@ukr.net>
2019-09-13 21:00:06 +0300

    Added should create to Employees

--- webapp/src/app/employees/dialogs/edit-employee/edit-employee.component.spec.ts
+++ webapp/src/app/employees/dialogs/edit-employee/edit-employee.component.spec.ts
@@ -1,14 +1,55 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Router} from '@angular/router';
 
 import {EditEmployeeComponent} from './edit-employee.component';
+import {Employee} from '../../employee';
+import {MaterialModule} from '../../../material/material.module';
+import {EmployeeService} from '../../employee.service';
 
 describe('EditEmployeeComponent', () => {
   let component: EditEmployeeComponent;
   let fixture: ComponentFixture<EditEmployeeComponent>;
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
+  const employee = new Employee();
+  employee.id = 123;
+  employee.firstName = 'Hank';
+  employee.lastName = 'Hill';
+  employee.gender = 'M';
+  employee.birthday = new Date(Date.now());
+  employee.city = 'Texas';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [EditEmployeeComponent]
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MaterialModule
+      ],
+      declarations: [
+        EditEmployeeComponent
+      ],
+      providers: [
+        EmployeeService,
+        {
+          provide: Router,
+          useValue: mockRouter
+        },
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {data: employee}
+        }
+      ]
     })
       .compileComponents();
   }));
@@ -21,5 +62,9 @@ describe('EditEmployeeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });
--- webapp/src/app/employees/employees.component.spec.ts
+++ webapp/src/app/employees/employees.component.spec.ts
@@ -1,19 +1,40 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {EmployeesComponent} from './employees.component';
+import {MaterialModule} from '../material/material.module';
+import {ProjectComponent} from '../projects/project.component';
+import {PageNotFoundComponent} from '../page-not-found/page-not-found.component';
+import {EmployeeService} from './employee.service';
+import {AddEmployeeComponent} from './dialogs/add-employee/add-employee.component';
+import {DeleteEmployeeComponent} from './dialogs/delete-employee/delete-employee.component';
+import {EditEmployeeComponent} from './dialogs/edit-employee/edit-employee.component';
 
 describe('EmployeesComponent', () => {
   let component: EmployeesComponent;
   let fixture: ComponentFixture<EmployeesComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EmployeesComponent]
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MaterialModule
+      ],
+      declarations: [
+        AddEmployeeComponent,
+        DeleteEmployeeComponent,
+        EditEmployeeComponent,
+        EmployeesComponent
+      ],
+      providers: [
+        EmployeeService
+      ]
     })
       .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(EmployeesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +42,9 @@ describe('EmployeesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });

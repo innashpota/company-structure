c4ce38a61499c0bc040c1ece04c2831d0e108abf
Inna <shpota_inna@ukr.net>
2019-09-18 21:00:16 +0300

    Added should create to PageNotFoundComponent

--- webapp/src/app/page-not-found/page-not-found.component.spec.ts
+++ webapp/src/app/page-not-found/page-not-found.component.spec.ts
@@ -1,19 +1,46 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {PageNotFoundComponent} from './page-not-found.component';
+import {MaterialModule} from '../material/material.module';
+import {AddEmployeeToProjectComponent} from '../projects/dialogs/add-employee-to-project/add-employee-to-project.component';
+import {AddProjectComponent} from '../projects/dialogs/add-project/add-project.component';
+import {DeleteProjectComponent} from '../projects/dialogs/delete-project/delete-project.component';
+import {EditEmployeeInProjectComponent} from '../projects/dialogs/edit-employee-in-project/edit-employee-in-project.component';
+import {EditProjectTitleComponent} from '../projects/dialogs/edit-project-title/edit-project-title.component';
+import {EmployeesComponent} from '../employees/employees.component';
+import {ProjectComponent} from '../projects/project.component';
+import {ProjectService} from '../projects/project.service';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
   let fixture: ComponentFixture<PageNotFoundComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PageNotFoundComponent]
+      imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MaterialModule
+      ],
+      declarations: [
+        AddEmployeeToProjectComponent,
+        AddProjectComponent,
+        DeleteProjectComponent,
+        EditEmployeeInProjectComponent,
+        EditProjectTitleComponent,
+        EmployeesComponent,
+        ProjectComponent,
+        PageNotFoundComponent
+      ],
+      providers: [
+        ProjectService
+      ]
     })
       .compileComponents();
-  }));
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,5 +48,9 @@ describe('PageNotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });
